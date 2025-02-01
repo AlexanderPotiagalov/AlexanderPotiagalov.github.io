d1ce257ddd6ed85a256f89d2e4ccaa89e4abdc79
AlexanderPotiagalov <apa168@sfu.ca>
2025-02-01 15:30:34 -0800

    Fix GitHub Pages deployment issue

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/AlexanderPotiagalov.github.io/", // <-- Add this line (Replace with your repo name)
   plugins: [react()],
+  base: "/AlexanderPotiagalov.github.io/",
 });
