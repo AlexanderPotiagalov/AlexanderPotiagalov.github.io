3e122e3f59338f2f9835916d5a070a1c77d6069e
AlexanderPotiagalov <apa168@sfu.ca>
2025-02-01 14:51:07 -0800

    Added Footer component and updated App.jsx & index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import React from "react";
 import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
 import "./index.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Footer />
     </div>
   );
 }
