5c492e573eced0283d1ab81ce744ccba0b7cc3f6
AlexanderPotiagalov <apa168@sfu.ca>
2025-02-01 17:42:07 -0800

    Created an Experience.jsx Component and added it to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./Header.jsx";
 import ProfilePicture from "./PortfolioPicture.jsx";
+import Experience from "./Experience.jsx";
 import Footer from "./Footer.jsx";
 import "./index.css";
 
@@ -10,6 +11,9 @@ function App() {
       <Header />
       <section id="profile-picture">
         <ProfilePicture />
+      </section>
+      <section id="experience">
+        <Experience />
       </section>
       <Footer />
     </div>
